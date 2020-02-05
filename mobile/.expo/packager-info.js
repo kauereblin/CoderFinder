9a14032e49f2a30cb546cf5edcc551f5119f7dd6
Kaue Reblin <49495681+kauereblin@users.noreply.github.com>
2020-02-04 23:05:46 -0300

    Create packager-info.js

---
+++ mobile/.expo/packager-info.js
@@ -0,0 +1,9 @@
+{
+  "devToolsPort": 19002,
+  "expoServerPort": 19000,
+  "packagerPort": 19001,
+  "packagerPid": 6852,
+  "expoServerNgrokUrl": "https://q2-gns.anonymous.mobile.exp.direct",
+  "packagerNgrokUrl": "https://packager.q2-gns.anonymous.mobile.exp.direct",
+  "ngrokPid": 2396
+}
